550dcf112d6191d3acd99c3d6157e724aa8ea8b9
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-02-02 17:22:27 -0500

    Created Food2Fork AJAX requests

--- app/assets/javascripts/apiQuery.js
+++ app/assets/javascripts/apiQuery.js
@@ -1,5 +1,42 @@
 $(document).on('ready page:load' function() {
 
+	apiKey = "b05c45fc8ed1a6c807983eaf1d30e6b0";
+
+	function findRecipes() {
+
+		var ingredients = "";
+		var url = "http://food2fork.com/api/search?"
+			+ "key=" + apiKey
+			+ "&q=" + ingredients; 
+
+		$.ajax({
+			type: "GET",
+			dataType: 'json',
+			cache: false,
+			url: url,
+			success: function (data) {
+				alert('success');
+				console.log(data);
+			}
+		});
+	}
+
+	function recipeDetails() {
+
+		var recipeId = $(this).data("recipe_id");
+		var url = "http://food2fork.com/api/search?"
+			+ "key=" + apiKey
+			+ "&rId=" + recipeId; 
+
+		$.ajax({
+			type: "GET",
+			dataType: 'json',
+			cache: false,
+			url: url,
+			success: function (data) {
+				alert('success');
+				console.log(data);
+			}
+		});
+	}
 });
-
-
