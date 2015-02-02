cc32e741ddf736642685a51c42557443b2a4c518
Philippe Schwyter <pschwyter90@gmail.com>
2015-02-02 17:23:16 -0500

    Added omniauth and devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_and_belongs_to_many :ingredients
 	has_many :comments
 	has_many :ratings
