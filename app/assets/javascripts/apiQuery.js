7b0063658617f947dd995b6262b455f97fcc6d5f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-02-02 16:48:52 -0500

    Removed BigOven API

--- app/assets/javascripts/apiQuery.js
+++ app/assets/javascripts/apiQuery.js
@@ -1,20 +1,5 @@
-$(document).on('ready' function() {
-	
-	function getRecipeJson() {
-        var apiKey = "dvxn3Sx834s9J8nO8CJSFZfPZ5KUmWFE";
-        var titleKeyword = "lasagna";
-        var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&title_kw="
-                  + titleKeyword 
-                  + "&api_key="+apiKey;
-        $.ajax({
-            type: "GET",
-            dataType: 'json',
-            cache: false,
-            url: url,
-            success: function (data) {
-                alert('success');
-                console.log(data);
-            }
-        });
-    }
+$(document).on('ready page:load' function() {
+
 });
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150202195331) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
