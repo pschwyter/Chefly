9ea681faa809ff13aa16dd81a1b9b4d56950430a
Philippe Schwyter <pschwyter90@gmail.com>
2015-02-02 15:10:48 -0500

    Migrated to postgresql, added pry and awesome_print gems

--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202193229) do
+ActiveRecord::Schema.define(version: 20150202195331) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "recipe_id"
   end
 
   create_table "ingredients", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "recipe_id"
   end
+
+  create_table "ingredients_users", id: false, force: true do |t|
+    t.integer "ingredient_id"
+    t.integer "user_id"
+  end
+
+  add_index "ingredients_users", ["ingredient_id"], name: "index_ingredients_users_on_ingredient_id", using: :btree
+  add_index "ingredients_users", ["user_id"], name: "index_ingredients_users_on_user_id", using: :btree
 
   create_table "ratings", force: true do |t|
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "recipe_id"
   end
 
   create_table "recipes", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "ingredient_id"
   end
 
   create_table "users", force: true do |t|
@@ -43,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150202193229) do
     t.string   "avatar"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "ingredient_id"
   end
 
 end
