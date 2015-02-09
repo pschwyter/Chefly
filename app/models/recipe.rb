47cdefd2ea0a763e05d8363b07c9c6756204ee68
Philippe Schwyter <pschwyter90@gmail.com>
2015-02-09 13:34:13 -0500

    Created cuisines and course models, updated associations to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,15 @@
 class Recipe < ActiveRecord::Base
 	has_and_belongs_to_many :ingredients
+	has_and_belongs_to_many :courses
+	has_and_belongs_to_many :cuisines
 	has_many :comments
 	has_many :ratings
 
-	def self.ingredient_search(ingredient)
-		if ingredient && ingredient != ''
-			Ingredient.where(name: ingredient).first.recipes
-		else
-			Recipe.all
-		end
-	end
+	# def self.ingredient_search(ingredient)
+	# 	if ingredient && ingredient != ''
+	# 		Ingredient.where(name: ingredient).first.recipes
+	# 	else
+	# 		Recipe.all
+	# 	end
+	# end
 end
