e32124f5e9eecc21f9ee245a6e17a9cd38134499
Philippe Schwyter <pschwyter90@gmail.com>
2015-02-11 17:11:28 -0500

    Redid swiping effects to add css animations

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	// $('#recipe-search').submit(function(e) {
 
 	// 	e.preventDefault();  
-
+	var recipeCount = 10;
 	var recipeGet = function() {
 		var ingredients = $('#ingredient-box div').map(function(index, div){return $(div).text()});
 		var ingredients_array = [];
@@ -26,6 +26,13 @@ $(document).ready(function(){
 	        dataType: "SCRIPT" // you want a difference between normal and ajax-calls, and json is standard
 	    }).fail(function(){
 	    	console.log('fail');
+	    }).success(function(){
+	    	console.log('success');
+	    	var recipeCount = $('.search-results > div').length;
+	    	var currentIndex = 0;
+    	  /* add the active class to the first item to hide all the others */
+	    	$('.search-results > div:eq(' + currentIndex + ')').addClass('active');
+	    	imageCallback();
 	    });
 	}
 
@@ -53,51 +60,23 @@ $(document).ready(function(){
 	$("#add-ingredient").on('click', callback);
 
 	var imageCallback = function() {
-		var imgRight = child +1;
-		var imgLeft = child -1;
+		var imgRight = currentIndex +2;
+		var imgLeft = currentIndex;
 
-		var numRecipes = $("#search-results").children().length;
-		if (imgRight > numRecipes) { imgRight = 1; }
-		if (imgLeft < 1) { imgLeft = numRecipes; }
+		var recipeCount = $(".search-results").children().length;
+		if (imgRight > recipeCount) { imgRight = 1; }
+		if (imgLeft < 1) { imgLeft = recipeCount; }
 
-		var imageUrlRight = $("#search-results .recipe-thumb:nth-child(" + imgRight + ") .inner img").attr('src');
-		$('#right-img').css('background-image', 'url(' + imageUrlRight + ')');
+		var imageUrlRight = $(".search-results .recipe-thumb:nth-child(" + imgRight + ") .inner img").attr('src');
+		$('.right-img').css('background-image', 'url(' + imageUrlRight + ')');
 
-		var imageUrlLeft = $("#search-results .recipe-thumb:nth-child(" + imgLeft + ") .inner img").attr('src');
-		$('#left-img').css('background-image', 'url(' + imageUrlLeft + ')');
+		var imageUrlLeft = $(".search-results .recipe-thumb:nth-child(" + imgLeft + ") .inner img").attr('src');
+		$('.left-img').css('background-image', 'url(' + imageUrlLeft + ')');
 	}
-
-
-	var child = 1;
-	$('#right-swipe').on('click', function() {
-		child ++;
-
-		var numRecipes = $("#search-results").children().length;
-		if (child > numRecipes) { child = 1; }
-
-		$("#search-results").children().hide();
-		$("#search-results .recipe-thumb:nth-child(" + child + ")").show();
-		$('.title').textfill({ maxFontPixels: 200 });
-
-		imageCallback();
-	});
-
-	$('#left-swipe').on('click', function() {
-		child --;
-
-		var numRecipes = $("#search-results").children().length;
-		if (child < 1) { child = numRecipes; }
-
-		$("#search-results").children().hide();
-		$("#search-results .recipe-thumb:nth-child(" + child + ")").show();
-		$('.title').textfill({ maxFontPixels: 200 });
-
-		imageCallback();
-	});
 
 	//Add hover class to recipe thumb when hovering over buttons
 
-	$("#right-swipe").hover(
+	$(".right-swipe").hover(
 		function() {
 			$(".recipe-thumb").addClass( "hover" );
 		}, function() {
@@ -105,13 +84,38 @@ $(document).ready(function(){
 		}
 		);
 
-	$("#left-swipe").hover(
+	$(".left-swipe").hover(
 		function() {
 			$(".recipe-thumb").addClass( "hover2" );
 		}, function() {
 			$(".recipe-thumb").removeClass( "hover2" );
 		}
 		);
+
+	// Transition out recipe result on clicking next
+	var currentIndex = 0;
+  $('.swipe').on('click', function() {
+  	console.log('CLICK');
+    var $active  = $('.search-results > div.active'),
+        isNext   = $(this).hasClass('right-swipe');
+    currentIndex = ((currentIndex + (isNext ? 1 : -1)) % recipeCount);
+
+     // go back to the last item if we hit -1 
+    if (currentIndex === -1) {
+      currentIndex = recipeCount - 1;
+    }
+
+    var $next = $('.search-results > div:eq(' + currentIndex + ')');
+    $active.addClass(isNext ? 'next-out' : 'prev-out');
+    $next.addClass('active').addClass(isNext ? 'next-in' : 'prev-in');
+    setTimeout(function() { 
+      $active.removeClass('active next-out prev-out');
+      $next.removeClass('next-in prev-in');
+      imageCallback();
+    }, 500);
+    return false;
+  });
+
 });
 
 
