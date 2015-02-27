9e82b59c56c477d7927afdb97784c76d1f7233d6
Philippe Schwyter <pschwyter90@gmail.com>
2015-02-27 16:22:20 -0500

    Replaced recipe favourite flip with modal

---
+++ app/assets/javascripts/recipe_modal.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(function() {
+    $("#modal-1").on("change", function() {
+      if ($(this).is(":checked")) {
+        $("body").addClass("modal-open");
+      } else {
+        $("body").removeClass("modal-open");
+      }
+    });
+
+    $(".modal-window").on("click", function() {
+      $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+      e.stopPropagation();
+    });
+  });
+
+})
