c47ee98cd280eb7ec41daf25d76aa11e6ff41d7f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-03-31 13:18:30 -0400

    Added info button to favourites page

--- app/assets/javascripts/fullPage.js
+++ app/assets/javascripts/fullPage.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-    	keyboardScrolling: true,
-    	verticalCentered: true
-    });
+	
+	$('#fullpage').fullpage({
+		keyboardScrolling: true,
+		verticalCentered: true
+	});
 });
