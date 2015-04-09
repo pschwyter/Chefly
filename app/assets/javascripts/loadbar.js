4d1e30e69269db7bcaa387d567e2d1a44e7c15c2
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-09 18:06:39 -0400

    Added load bar ajax script

---
+++ app/assets/javascripts/loadbar.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+
+// 	$.ajaxSetup({
+// 	    beforeSend:function(){
+// 	        // show gif here, eg:
+// 	        $("#loading").show();
+// 	        // $(".search-results").hide();
+// 	    },
+// 	    complete:function(){
+// 	        // hide gif here, eg:
+// 	        $("#loading").hide();
+// 	        // $(".search-results").show();
+// 	    }
+// 	});
+
+// });
