ccdb97cc835b6a05e278a4ea00078dc151e78fda
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-09 15:06:07 -0400

    removed full page js

--- app/assets/javascripts/form_flip.js
+++ app/assets/javascripts/form_flip.js
@@ -1,7 +1,9 @@
-$(document).ready(function(){
-	$('.recipe-thumb').on('click', function(){
-		$('.inner').toggleClass('flipped');
-	});
-
-	
-});
+// $(document).ready(function(){
+// 	$('.recipe-thumb').on('click', function(e){
+// 		var target = $(e.target);
+// 		console.log(target);
+// 		if(!target.is('.top-heart, .top-heart *, .bottom-heart, .bottom-heart *') {
+// 			$('.inner').toggleClass('flipped');
+// 		}
+// 	});
+// });
--- app/assets/javascripts/fullPage.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	
-	$('#fullpage').fullpage({
-		keyboardScrolling: true,
-		verticalCentered: true
-	});
-});
